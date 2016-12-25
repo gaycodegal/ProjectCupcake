1f588f3568fa3ee8fbc5c75172034f841e772198
stephoro <stephenjoro@gmail.com>
2016-12-24 19:12:25 -0500

    MacOS Project Upload
    Updated So that MacOS now has a project. Android and Windows to follow.

---
+++ Cupcake/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Cupcake
+//
+//  Created by Steph on 12/24/16.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
